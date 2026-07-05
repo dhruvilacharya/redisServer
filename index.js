eebe9fc8051056e69622a76d23ae6e4221aef684
dhruvilacharya <dhruvilacharya3007@gmail.com>
2026-07-05 10:41:44 +0530

    first push

---
+++ index.js
@@ -0,0 +1,44 @@
+const net = require('net');
+const Parser = require('redis-parser')
+const store = {}
+
+//creating a tcp server
+const server = net.createServer(connection => {
+    console.log('Client connected...')
+
+    //accepting commands
+    connection.on('data', data => {
+
+        const parser = new Parser({
+            returnReply: (reply) => {
+                const command = reply[0];
+
+                switch(command) {
+                    case 'set': {
+                        const key = reply[1];
+                        const value = reply[2];
+                        store[key] = value;
+                        connection.write('+OK\r\n')
+                    }
+                    break;
+                    case 'get': {
+                        const key = reply[1];
+                        console.log({key, value: store[key]})
+                        const[value] = store[key];
+
+                        if(!value) connection.write('$-1\r\n')
+                        else connection.write('$${value.length}\r\n${value}\r\n')
+                    }
+                }
+            },
+            returnError: (err) => {
+                console.log('=>', err);
+            }
+        })
+        
+        parser.execute(data);
+        connection.write('+OK\r\n')
+    })
+})
+
+server.listen(8000, () => console.log('Custom redis server running on port 8000'))
